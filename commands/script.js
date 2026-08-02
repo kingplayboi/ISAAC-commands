2391213efd9daf809cf6d74b0575a8898120b8b7
kingplayboi <isaacmaingi067@gmail.com>
2026-08-03 00:19:24 +0300

    Add second pairing site link to script.js

--- commands/script.js
+++ commands/script.js
@@ -31,6 +31,7 @@ Hello 👋 *${senderName},*
 ↳ https://github.com/kingplayboi
 
 🔗 *WhatsApp Pairing:*
+↳ https://kingpin-sjlx.onrender.com/
 ↳ https://kingpin-sjlx.onrender.com
 ★ Save your Session-ID!
 
