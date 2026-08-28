82364b0a2acb5819f5c92ae71b4025fc042fd255
kingplayboi <isaacmaingi067@gmail.com>
2026-08-28 16:42:41 +0300

    ISAAC

--- commands/restart.js
+++ commands/restart.js
@@ -2,14 +2,15 @@ const { isOwner } = require('../utils/isOwner');
 
 module.exports = {
     name: 'restart',
+    aliases: ['reboot'],
     description: 'Restarts the bot process (owner only).',
     async execute(sock, msg) {
         const jid = msg.key.remoteJid;
         if (!isOwner(msg)) {
-            return sock.sendMessage(jid, { text: '❌ Only the bot owner can use this command.' }, { quoted: msg });
+            return sock.sendMessage(jid, { text: '*❌ Are you the owner ?.*' }, { quoted: msg });
         }
 
-        await sock.sendMessage(jid, { text: '🔄 Restarting...' }, { quoted: msg });
+        await sock.sendMessage(jid, { text: '*🔄 Restarting...*' }, { quoted: msg });
         setTimeout(() => process.exit(0), 1000);
     },
 };
