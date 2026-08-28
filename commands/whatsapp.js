14e0b4701c474723efda1ec5a1414da9e14029cd
kingplayboi <isaacmaingi067@gmail.com>
2026-08-28 23:40:27 +0300

    Add del command to whatsapp commands

--- commands/whatsapp.js
+++ commands/whatsapp.js
@@ -62,40 +62,40 @@ module.exports = [
   },
 
   {
-    name: 'del',
-    aliases: ['delete'],
-    description: 'Delete a message. Reply to a message with .del',
-    async execute(sock, msg) {
-      const { jid, quotedMessage, quotedKey } = getQuoted(sock, msg);
+  name: 'del',
+  aliases: ['delete'],
+  description: 'Delete a message. Reply to a message with .del',
+  async execute(sock, msg) {
+    const { jid, quotedMessage, quotedKey } = getQuoted(sock, msg);
 
-      if (!quotedMessage) {
+    if (!quotedMessage) {
+      return sock.sendMessage(jid, {
+        text: '*❌ Which message should I delete ?*'
+      }, { quoted: msg });
+    }
+
+    if (jid.endsWith('@g.us') && !quotedKey.fromMe) {
+      const groupMetadata = await sock.groupMetadata(jid);
+      const botJid = (sock.user?.id || '').split(':')[0] + '@s.whatsapp.net';
+
+      const botParticipant = groupMetadata.participants.find(
+        participant => participant.id === botJid
+      );
+
+      const isBotAdmin =
+        botParticipant &&
+        (botParticipant.admin === 'admin' || botParticipant.admin === 'superadmin');
+
+      if (!isBotAdmin) {
         return sock.sendMessage(jid, {
-          text: '*❌ Which message should I delete ?*'
+          text: '*❌ Are you an admin ???*'
         }, { quoted: msg });
       }
-
-      if (jid.endsWith('@g.us')) {
-        const groupMetadata = await sock.groupMetadata(jid);
-        const botJid = (sock.user?.id || '').split(':')[0] + '@s.whatsapp.net';
-
-        const botParticipant = groupMetadata.participants.find(
-          participant => participant.id === botJid
-        );
-
-        const isBotAdmin =
-          botParticipant &&
-          (botParticipant.admin === 'admin' || botParticipant.admin === 'superadmin');
-
-        if (!isBotAdmin) {
-          return sock.sendMessage(jid, {
-            text: '*❌ I need admin privileges.*'
-          }, { quoted: msg });
-        }
-      }
-
-      await sock.sendMessage(jid, { delete: quotedKey });
     }
-  },
+
+    await sock.sendMessage(jid, { delete: quotedKey });
+  }
+},
 
   {
     name: 'react',
